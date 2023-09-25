0dfee1fc34f30f844692291150f1fcdc2aa9c59a
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-25 20:53:48 +0700

    delete user fields from models

--- apps/events/models.py
+++ apps/events/models.py
@@ -7,9 +7,7 @@ class Event(models.Model):
     title = models.CharField('Название', max_length=50, unique=True)
     description = models.CharField('Описание', max_length=512)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class EventFollower(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    follower = models.ForeignKey(User, models.SET_NULL, null=True)
