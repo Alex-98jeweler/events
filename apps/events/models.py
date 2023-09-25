c825364c192d2963cc7f853c971161bddb0f77cd
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-26 01:23:53 +0700

    edit events and eventfollowers models

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
 
 class Event(models.Model):
     
     title = models.CharField('Название', max_length=50, unique=True)
     description = models.CharField('Описание', max_length=512)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
 
 class EventFollower(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
