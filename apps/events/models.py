446ab2ed1c5725a5d7e74a9477a9c9a653951cd4
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-25 20:09:18 +0700

    edit Event model and make initial migrations for event app

--- apps/events/models.py
+++ apps/events/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     
     title = models.CharField('Название', max_length=50, unique=True)
-    description = models.CharField('Описание')
+    description = models.CharField('Описание', max_length=512)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
