bada72895fc88541adbc3b4c5d7a3c84d6f14bcf
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-26 16:49:39 +0700

    edit user serializer

--- apps/custom_user/serializers.py
+++ apps/custom_user/serializers.py
@@ -9,8 +9,14 @@ class UserSerializer(serializers.Serializer):
     last_name = serializers.CharField()
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    birthday = serializers.DateField()
 
     def to_internal_value(self, data):
         data = super().to_internal_value(data)
 
         return services.UserDataClass(**data)
+    
+    class Meta:
+        extra_kwargs = {
+            'birthday': {"required": False}
+        }
