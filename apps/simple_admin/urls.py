ba6ed4ae59f71ba3ffec3c9f3bf173ff5e09c780
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-27 06:27:24 +0700

    add urls for event-list

--- apps/simple_admin/urls.py
+++ apps/simple_admin/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('dashboard/', views.DashboardView.as_view()),
     path('logout/', views.logout_view, name='logout'),
-    path('registration/', views.RegistrationView.as_view())
+    path('registration/', views.RegistrationView.as_view()),
+    path('events/', views.EventListView.as_view(), name='event-list')
     
 ]
