bddac21f5064c4c9146811194ec6993a825594d1
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-26 22:34:59 +0700

    implement login, logout logic

--- apps/simple_admin/urls.py
+++ apps/simple_admin/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('dashboard/', views.DashboardView.as_view()),
+    path('logout/', views.logout_view, name='logout'),
+    
 ]
--- apps/simple_admin/views.py
+++ apps/simple_admin/views.py
@@ -1,21 +1,33 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-class DashboardView(View):
-    pass
+class DashboardView(LoginRequiredMixin, View):
+    login_url = "/login/"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name='simple_admin/dashboard.html')
 
 
 class LoginView(LoginView):
     
     template_name = "simple_admin/login.html"
     
-    def get_success_url(self) -> str:
-        return super().get_success_url()
+    def get_redirect_url(self) -> str:
+        return "/dashboard/"
     
-
+    
+    
+def logout_view(request):
+    logout(request)
+    return redirect("/login/")
+    
 
 class RegistrationView(View):
     pass
