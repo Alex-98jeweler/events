5891d82d5ae06a3ef57bc7878155fdd6ff976279
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-27 01:00:10 +0700

    add creation form for user

---
+++ apps/custom_user/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'birthday')
+    
+    
