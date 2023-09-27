579bc4731b9a637a63c3d3e7407c812d60b3bb9c
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-27 12:10:00 +0700

    add pagination for event list api

--- apps/events/views.py
+++ apps/events/views.py
@@ -4,13 +4,14 @@ from rest_framework import permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from . import serializers, models
-
+from .pagination import Paginator
 
 class ListCreateEventAPIView(generics.ListCreateAPIView):
     
     serializer_class = serializers.EventSerializers
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+    pagination_class = Paginator
 
     def get_queryset(self):
         return models.Event.objects.all()
