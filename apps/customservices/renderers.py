dcd96b585785c84405dfaa2c49234392c594fd35
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-27 10:13:12 +0700

    edit custom renderer

--- apps/customservices/renderers.py
+++ apps/customservices/renderers.py
@@ -6,9 +6,16 @@ class JSONResponseRenderer(JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        errors = []
+        errors = {}
+        print(data)
+        if 'detail' in data:
+            errors['detail'] = data['detail']
+            del data['detail']
+        if 'code' in data:
+            errors['code'] = data['code']
+            del data['code']
         if isinstance(data, dict) and (error := data.get('errors')):
-            errors.extend(error)
+            errors['errors_detailed'] = error
             del data['errors']
         response_dict = {
             'result': data if data else {},
