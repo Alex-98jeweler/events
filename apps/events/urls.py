85b3790660800bb14574f1608d226af130ef74ca
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-29 00:46:11 +0700

    add follow/unfollow logic for api

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('event/', views.ListCreateEventAPIView.as_view(), name='list-create-event'),
     path('event/<int:pk>/', views.UpdateEventAPIView.as_view(), name='update-event'),
-    path('event/<int:pk>/', views.DestroyEventAPIView.as_view(), name='destroy-event')
+    path('event/<int:pk>/', views.DestroyEventAPIView.as_view(), name='destroy-event'),
+    path('event/<int:pk>/follow/', views.follow_event, name='follow-event')
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from rest_framework import decorators
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -36,3 +39,27 @@ class DestroyEventAPIView(generics.RetrieveDestroyAPIView):
     
     def get_queryset(self):
         return models.Event.objects.all()
+
+
+@decorators.api_view(http_method_names=['POST', ])
+def follow_event(request, *args, **kwargs):
+    event_id = kwargs.get('pk')
+    user = request.user
+    event = models.Event.objects.get(pk=event_id)
+    try:
+        models.EventFollower.objects.create(event=event, follower=user)
+    except IntegrityError as e:
+        return Response(data={'message': "You are already followed"}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(data={'message': 'successfully followed'}, status=status.HTTP_200_OK)
+    
+@decorators.api_view(http_method_names=['POST', ])
+def unfollow_event(request, *args, **kwargs):
+    event_id = kwargs.get('pk')
+    user = request.user
+    event = models.Event.objects.get(pk=event_id)
+    try:
+        event_follower = models.EventFollower.objects.get(event=event, follower=user)
+        event_follower.delete()
+    except Exception as e:
+        return Response(data={'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(data={'message': 'successfully followed'}, status=status.HTTP_200_OK)
