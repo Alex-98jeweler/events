1ee82edf33323f36367537824c9b6b4bdceb061e
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-09-28 20:56:13 +0700

    prepare docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:3.8.10
+FROM python:3.8.10-alpine
 
 WORKDIR /usr/src/app
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-RUN apt update
-RUN apt install postgresql gcc python3-dev musl-dev -y
+RUN apk update \
+    && apk add postgresql-dev gcc python3-dev musl-dev libffi-dev
 
 RUN pip install --upgrade pip
 COPY ./requirements.txt .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
-if [ "$DATABASE" = "postgres" ]
-then
+#!/bin/sh
 
-    while ! nc -z $DB_HOST $POSTGRES_PORT; do
-      sleep 0.1
-    done
-fi
+# if [ "$DATABASE" = "postgres" ]
+# then
+
+#     while ! nc -vz $POSTGRES_HOST $POSTGRES_PORT; do
+#       sleep 0.1
+#       echo "here"
+#     done
+# fi
 
 python manage.py flush --no-input
 python manage.py migrate
